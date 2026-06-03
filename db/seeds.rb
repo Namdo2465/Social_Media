a64a699e336d694d1e59a64a0764f9f574234cad
NamDo <166409645+Namdo2465@users.noreply.github.com>
2026-06-03 11:57:23 +0700

    Add seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,54 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Like.destroy_all
+Comment.destroy_all
+Post.destroy_all
+FollowRequest.destroy_all
+Profile.destroy_all
+User.destroy_all
+
+users = 5.times.map do
+  user = User.create!(
+    email: Faker::Internet.email,
+    password: "password"
+  )
+
+  user.profile.update!(
+    name: Faker::Name.name,
+    bio: Faker::Lorem.sentence
+  )
+
+  user
+end
+
+users.each do |user|
+  3.times do
+    user.posts.create!(
+      content: Faker::Lorem.paragraph
+    )
+  end
+end
+
+Post.all.each do |post|
+  users.sample(2).each do |user|
+    post.comments.create!(
+      user: user,
+      content: Faker::Lorem.sentence
+    )
+
+    post.likes.find_or_create_by!(
+      user: user
+    )
+  end
+end
+
+users[0].sent_follow_requests.create!(
+  receiver: users[1],
+  status: "accepted"
+)
+
+users[0].sent_follow_requests.create!(
+  receiver: users[2],
+  status: "accepted"
+)
